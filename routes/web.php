8e20322c11a4b98ba7a74fe433c22db43e773ab1
cgiaccone-tt <christopher.giaccone@gmail.com>
2024-02-29 09:08:43 -0500

    Installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\FallbackController;
-use Barryvdh\Debugbar\Facades\Debugbar;
-use Barryvdh\Debugbar\Twig\Extension\Debug;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,24 +15,17 @@ use App\Http\Controllers\PostsController;
 */
 
 Route::get('/', function () {
-    Debugbar::error('Hello');
     return view('welcome');
 });
-//GET
-Route::get('/blog', [PostsController::class, 'index'])->name('blog.index');
-Route::get('/blog/{id}', [PostsController::class, 'show']);
 
-//POST
-Route::get('/blog/create', [PostsController::class, 'create']);
-Route::post('/blog', [PostsController::class, 'store']);
-//PUT OR PATCH
-Route::get('/blog/{id}/edit', [PostsController::class, 'edit']);
-Route::patch('/blog/{id}', [PostsController::class, 'update']);
-//DELETE
-Route::delete('/blog/{id}', [PostsController::class, 'destroy']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::fallback(FallbackController::class);
-
-//Route::get('/', HomeController::class);
-//Route::any('/blog', [PostsController::class, 'index']);
+require __DIR__.'/auth.php';
